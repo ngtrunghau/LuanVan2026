1518872236a20cea5c0d734b4a89351dfebd0320
nguyentrunghau <101037907+ngtrunghau@users.noreply.github.com>
2026-04-05 23:51:55 +0700

    Update notifyModel.js

--- FE/src/models/notifyModel.js
+++ FE/src/models/notifyModel.js
@@ -1,7 +1,8 @@
 const addMessage = (item) => {
+    const safeItem = item || {};
     return {
-        message: item.message,
-        code: item.code
-    }
+        message: safeItem.message || "Có lỗi xảy ra",
+        code: safeItem.code ?? -1
+    };
 }
 export const notifyModel = {addMessage};
